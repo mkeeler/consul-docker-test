2bed99da2cfda26846573ed162307af067850b5c
Matt Keeler <mjkeeler7@gmail.com>
2022-08-03 18:15:51 -0400

    Add mesh = “write” permissions to gateways

--- peering/clients/gateway-acls.tf
+++ peering/clients/gateway-acls.tf
@@ -1,3 +1,31 @@
+locals {
+  alphaGatewayPartitions = toset([
+    for key, gw in local.alphaGateways :
+    gw.partition
+  ])
+
+  betaGatewayPartitions = toset([
+    for key, gw in local.betaGateways :
+    gw.partition
+  ])
+}
+
+resource "consul_acl_policy" "alphaMeshWrite" {
+  for_each  = local.alphaGatewayPartitions
+  provider  = consul.alpha
+  partition = each.value
+  name      = "mesh-write"
+  rules     = "mesh = \"write\""
+}
+
+resource "consul_acl_policy" "betaMeshWrite" {
+  for_each  = local.betaGatewayPartitions
+  provider  = consul.beta
+  partition = each.value
+  name      = "mesh-write"
+  rules     = "mesh = \"write\""
+}
+
 resource "consul_acl_token" "alphaGatewayTokens" {
   for_each  = local.alphaGateways
   provider  = consul.alpha
@@ -5,6 +33,7 @@ resource "consul_acl_token" "alphaGatewayTokens" {
   service_identities {
     service_name = "mesh-gateway"
   }
+  policies = [consul_acl_policy.alphaMeshWrite[each.value.partition].name]
 }
 
 data "consul_acl_token_secret_id" "alphaGatewaySecrets" {
@@ -21,6 +50,7 @@ resource "consul_acl_token" "betaGatewayTokens" {
   service_identities {
     service_name = "mesh-gateway"
   }
+  policies = [consul_acl_policy.betaMeshWrite[each.value.partition].name]
 }
 
 data "consul_acl_token_secret_id" "betaGatewaySecrets" {
